c317868a9446a5b3f8d2c1ffea445ce38df82fea
gumsu <chavi9@naver.com>
2021-01-13 00:04:06 +0900

    Initial commit

---
+++ app/src/main/java/com/gdh/daangn_market_used_trading/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gdh.daangn_market_used_trading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
